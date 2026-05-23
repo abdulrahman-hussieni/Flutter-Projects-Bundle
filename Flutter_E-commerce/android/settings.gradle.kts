d229bf7bed84121e18cb9e6f6843a18e4d24a425
abdelrahman-hossieny <abdohossieny@gmail.com>
2026-05-24 00:58:18 +0300

    Fix: Move leaked E-commerce files into its proper folder

--- Flutter_E-commerce/android/settings.gradle.kts
+++ Flutter_E-commerce/android/settings.gradle.kts
@@ -19,6 +19,21 @@ pluginManagement {
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "8.7.3" apply false
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+    // START: FlutterFire Configuration
+    id("com.google.gms.google-services") version("4.3.15") apply false
+    // END: FlutterFire Configuration
+=======
+>>>>>>> temp-hotel/main
+=======
+>>>>>>> temp-todo/main
+=======
+>>>>>>> temp-weather/main
+=======
+>>>>>>> temp-eco/main
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
 }
 
